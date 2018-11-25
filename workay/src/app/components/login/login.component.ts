10b359217ece86fb4acdef27e2a9426f2e4cbe24
vmedvedchuk <victor.medvedchuk@gmail.com>
2018-11-25 14:48:17 +0200

    added registration

--- workay/src/app/components/login/login.component.ts
+++ workay/src/app/components/login/login.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  loginForm = true;
   constructor() { }
 
   ngOnInit() {
   }
-
+  signUp() {
+    this.loginForm = false;
+  }
 }
